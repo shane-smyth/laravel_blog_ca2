57905ec9e1a5ebc1de6edc8d32b7e7e1c3b7986f
shane-smyth <158466904+shane-smyth@users.noreply.github.com>
2025-02-22 12:19:04 +0000

    blog show.blade.php now shows the blogs image, tha authors details, and is responsive

--- resources/views/blog/show.blade.php
+++ resources/views/blog/show.blade.php
@@ -1,22 +1,44 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="w-4/5 m-auto text-left">
-    <div class="py-15">
-        <h1 class="text-6xl">
-            {{ $post->title }}
-        </h1>
+{{--    https://tailwindflex.com/@ron-hicks/blog-page-template--}}
+    <div class="max-w-screen-xl mx-auto p-5 sm:p-10 md:p-16 relative">
+        <div class="bg-cover bg-center text-center overflow-hidden"
+             style="min-height: 500px; background-image: url('{{ asset('images/' . $post->image_path) }}')"
+             title="{{ $post->title }}">
+        </div>
+        <div class="max-w-3xl mx-auto">
+            <div
+                class="mt-3 bg-white rounded-b lg:rounded-b-none lg:rounded-r flex flex-col justify-between leading-normal">
+                <div class="bg-white relative top-0 -mt-32 p-5 sm:p-10">
+                    <h1 href="#" class="text-gray-900 font-bold text-3xl mb-2">{{ $post->title }}</h1>
+                    <div class="flex items-center">
+                        <div class="flex-shrink-0">
+                            <a href="#">
+                                <span class="sr-only">{{ $post->user->name }}</span>
+                                <img class="h-10 w-10 rounded-full" src="https://avatarfiles.alphacoders.com/337/337251.jpg" alt="">
+                            </a>
+                        </div>
+                        <div class="ml-3">
+                            <p class="text-sm font-medium text-gray-900">
+                                <a href="#" class="hover:underline">{{ $post->user->name }}</a>
+                            </p>
+                            <div class="flex space-x-1 text-sm text-gray-500">
+                                {{ date('jS M Y', strtotime($post->updated_at)) }}
+                                <span aria-hidden="true"></span>
+                                <span aria-hidden="true"></span>
+                                <span aria-hidden="true"></span>
+                                <span aria-hidden="true"></span>
+                                <span>6 min read</span>
+                            </div>
+                        </div>
+                    </div>
+                    <p class="text-base leading-8 my-5">
+                        {{ $post->description }}
+                    </p>
+                </div>
+
+            </div>
+        </div>
     </div>
-</div>
-
-<div class="w-4/5 m-auto pt-20">
-    <span class="text-gray-500">
-        By <span class="font-bold italic text-gray-800">{{ $post->user->name }}</span>, Created on {{ date('jS M Y', strtotime($post->updated_at)) }}
-    </span>
-
-    <p class="text-xl text-gray-700 pt-8 pb-10 leading-8 font-light">
-        {{ $post->description }}
-    </p>
-</div>
-
-@endsection 
+@endsection
